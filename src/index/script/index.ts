49c82bf60e43d8574000e82a1b06e72cf1a9ad6f
kota <kota@imgsrc.co.jp>
2020-08-25 20:36:19 +0900

    update: スライドを追加

--- src/index/script/index.ts
+++ src/index/script/index.ts
@@ -1,13 +1,27 @@
 import gsap from 'gsap';
 import Swiper from 'swiper';
 
-import fragmentShader from './gl/fragmentShader.frag';
-
-console.log(fragmentShader);
-console.log(Swiper);
+// import fragmentShader from './gl/fragmentShader.frag';
+// console.log(Swiper);
 
 (() => {
   window.addEventListener('DOMContentLoaded', () => {
+    const el = document.getElementById('js-swiper');
+    const swiper = new Swiper(el, {
+      slidesPerView: 3,
+      loopAdditionalSlides: 3,
+      centeredSlides: true,
+      allowTouchMove: false,
+      speed: 2000,
+      autoplay: {
+        delay: -100
+      },
+      loop: true
+    });
+
+    console.log(el);
+
     console.log(gsap);
+    console.log(swiper);
   });
 })();
