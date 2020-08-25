a782013631288e3613157ad046129b216c480f42
kota <kota@imgsrc.co.jp>
2020-08-26 02:11:02 +0900

    add: メニューを作成

--- src/index/script/index.ts
+++ src/index/script/index.ts
@@ -5,12 +5,28 @@ import Swipe from './modules/_swiper';
 
 (() => {
   window.addEventListener('DOMContentLoaded', () => {
+    const toggleWrap = document.getElementById('js-toggle-wrap');
+    const toggle = document.getElementById('js-toggle');
+    const modal = document.getElementById('js-modal');
+
     const swiper = new Swipe();
 
     swiper.init();
 
     window.addEventListener('resize', () => {
       swiper.onResize();
+    });
+
+    toggleWrap.addEventListener('click', () => {
+      toggle.classList.toggle('open');
+
+      const isModal = toggle.classList.contains('open');
+
+      if (isModal) {
+        modal.classList.add('open');
+      } else {
+        modal.classList.remove('open');
+      }
     });
   });
 })();
