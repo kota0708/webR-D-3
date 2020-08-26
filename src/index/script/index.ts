1253f0db3179c53af4ce138c52f0b40b0f20b383
kota <kota@imgsrc.co.jp>
2020-08-26 12:20:57 +0900

    update: メニュー完成

--- src/index/script/index.ts
+++ src/index/script/index.ts
@@ -1,17 +1,12 @@
 import Swipe from './modules/_swiper';
 import Event from './modules/_event';
 import Menu from './modules/_menu';
-import Circle from './modules/_circle';
-
-// import fragmentShader from './gl/fragmentShader.frag';
-// console.log(Swiper);
 
 (() => {
   window.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swipe();
     const event = new Event();
     const menu = new Menu();
-    // const circle = new Circle();
 
     swiper.init();
     event.init();
