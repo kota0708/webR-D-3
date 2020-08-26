9a8ddef9ac97a58f12f350931c5e81bf146878ae
kota <kota@imgsrc.co.jp>
2020-08-26 15:11:48 +0900

    add: ローディング作成

--- src/index/script/modules/_swiper.ts
+++ src/index/script/modules/_swiper.ts
@@ -1,20 +1,35 @@
 import Swiper from 'swiper';
 
+import Circle from './_circle';
+
+import { makeArray } from '../../../shared/scripts/make-array';
+
 class Swipe {
   private elModal: HTMLElement;
+  private elLoaderWrap: HTMLElement;
   private elFooter: HTMLElement;
+  private elLoaders: HTMLElement[];
 
   private timer: number;
 
   private swiperModal: Swiper;
   private swiperFooter: Swiper;
+  private circle: Circle;
+
+  private swiperLoaders: Swiper[];
 
   constructor() {
     this.elModal = document.getElementById('js-swiper-modal');
     this.elFooter = document.getElementById('js-swiper-footer');
+    this.elLoaderWrap = document.getElementById('js-loader-wrap');
+    this.elLoaders = makeArray(document.querySelectorAll('.js-swiper-Loading'));
     this.timer = -1;
 
+    this.swiperLoaders = [];
+
     this.onResize = this.onResize.bind(this);
+
+    this.circle = new Circle();
   }
 
   public init(): void {
@@ -55,6 +70,43 @@ class Swipe {
       },
       loop: true
     });
+
+    this.elLoaders.forEach((r: HTMLElement, i: number) => {
+      const slidesPerView = Number(r.dataset.slidesperview);
+      const speed = Number(r.dataset.speed);
+
+      const swipe = new Swiper(r, {
+        slidesPerView,
+        loopAdditionalSlides: 3,
+        centeredSlides: true,
+        allowTouchMove: false,
+        speed,
+        autoplay: {
+          delay: 0
+        },
+        breakpoints: {
+          1130: {
+            slidesPerView: slidesPerView + 0.6,
+            loop: true
+          },
+          760: {
+            slidesPerView: slidesPerView + 0.3,
+            loop: true
+          },
+          450: {
+            slidesPerView,
+            loop: true
+          }
+        },
+        loop: true
+      });
+
+      this.swiperLoaders[i] = swipe;
+    });
+
+    window.setTimeout(() => {
+      this.circle.onAnimation(() => this.onDestory());
+    }, 2000);
   }
 
   public onResize(): void {
@@ -71,6 +123,14 @@ class Swipe {
     this.timer = window.setTimeout(() => {
       this.swiperModal.autoplay.start();
     }, 500);
+  }
+
+  private onDestory(): void {
+    this.elLoaderWrap.classList.add('close');
+
+    this.swiperLoaders.forEach((r: Swiper) => {
+      r.autoplay.stop();
+    });
   }
 }
 
